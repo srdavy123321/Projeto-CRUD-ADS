d6e56953ade96c2d05a9f61459d835299da4eb3d
FVS Dost <davyguimaraesico@gmail.com>
2019-11-05 20:56:35 -0200

    CRUD Inicial

---
+++ ProjetoCRUDADS/src/univs/edu/usuario/Usuario.java
@@ -0,0 +1,48 @@
+
+package univs.edu.usuario;
+
+
+public class Usuario {
+    
+    private int idUsuario;
+    
+    private String nomeUsuario;
+    
+    private String login;
+    
+    private String senha;
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+}
