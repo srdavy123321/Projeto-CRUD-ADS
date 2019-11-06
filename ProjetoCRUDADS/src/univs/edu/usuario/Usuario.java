82b32af421f8735c3e3c97548fdb0d4423f8b4b8
FVS Dost <davyguimaraesico@gmail.com>
2019-11-06 20:51:44 -0200

    Modelo inicial

--- ProjetoCRUDADS/src/univs/edu/usuario/Usuario.java
+++ ProjetoCRUDADS/src/univs/edu/usuario/Usuario.java
@@ -1,15 +1,28 @@
 
 package univs.edu.usuario;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+@Entity
 public class Usuario {
+    
+    @Id
+    @GeneratedValue
+    
     
     private int idUsuario;
     
+    @Column(length = 100, nullable = false)
+    
     private String nomeUsuario;
     
+     @Column(length = 100, nullable = false) 
     private String login;
-    
+     
+      @Column(length = 100, nullable = false, unique = true)
     private String senha;
 
     public int getIdUsuario() {
